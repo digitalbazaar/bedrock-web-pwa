c9413ec55dc4bc793cd48a2ec8b3ce9a0962eb87
Dave Longley <dlongley@digitalbazaar.com>
2022-04-21 12:56:42 -0400

    Add initial files.

--- test/web/10-api.js
+++ test/web/10-api.js
@@ -1,104 +1,59 @@
 /*!
- * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
-import {config, extend, isObject, ready, start, waitUntil} from '@bedrock/web';
+import {initialize, showInstallPrompt, state} from '@bedrock/web-pwa';
 
-describe('bedrock-web API', () => {
-  describe('API', () => {
-    it('should wait for a promise to resolve', async () => {
-      let waited = false;
-      waitUntil(new Promise(resolve => setTimeout(() => {
-        waited = true;
-        resolve();
-      }), 100));
-      await start();
-      waited.should.equal(true);
-    });
-    it('exports the proper APIs', async () => {
-      should.exist(config);
-      config.should.be.an('object');
-      should.exist(extend);
-      extend.should.be.a('function');
-      should.exist(isObject);
-      isObject.should.be.a('function');
-      should.exist(start);
-      start.should.be.instanceof(Function);
-      should.exist(ready);
-      ready.should.be.instanceof(Promise);
-      should.exist(waitUntil);
-      waitUntil.should.be.instanceof(Function);
-    });
-    it('should shallow extend an object', async () => {
-      const target = {a: 1, foo: {bar: 1}, baz: {x: 1}};
-      const source = {foo: {bar: 2}, baz: {y: 2}};
-      const result = extend({target, source});
-      const expect = {
-        a: 1, foo: {bar: 2}, baz: {y: 2}
-      };
-      result.should.deep.equal(expect);
-      target.should.deep.equal(expect);
-    });
-    it('should deep extend an object', async () => {
-      const target = {a: 1, foo: {bar: 1}, baz: {x: 1}};
-      const source = {foo: {bar: 2}, baz: {y: 2}};
-      const result = extend({target, source, deep: true});
-      const expect = {
-        a: 1, foo: {bar: 2}, baz: {x: 1, y: 2}
-      };
-      result.should.deep.equal(expect);
-      target.should.deep.equal(expect);
-    });
-    it('should deep extend an object with array source', async () => {
-      const target = {a: 1, foo: {bar: 1}, baz: {x: 1}};
-      const source = [{foo: {bar: 2}, baz: {y: 2}}];
-      const result = extend({target, source, deep: true});
-      const expect = {
-        a: 1, foo: {bar: 2}, baz: {x: 1, y: 2}
-      };
-      result.should.deep.equal(expect);
-      target.should.deep.equal(expect);
-    });
-    it('should deep extend an object with no existing key', async () => {
-      const target = {a: 1, foo: {bar: 1}};
-      const source = {foo: {bar: 2}, baz: {y: 2}};
-      const result = extend({target, source, deep: true});
-      const expect = {
-        a: 1, foo: {bar: 2}, baz: {y: 2}
-      };
-      result.should.deep.equal(expect);
-      target.should.deep.equal(expect);
-    });
-    it('should fail to extend a non-object', async () => {
-      const target = {};
-      const source = 'non-object';
-      let err;
+describe('bedrock-web-pwa', () => {
+  beforeEach(() => {
+    // clear initialized state
+    state._initialized = false;
+  });
+  describe('initialize', () => {
+    it('should pass', async () => {
+      let error;
       try {
-        extend({target, source});
+        initialize();
       } catch(e) {
-        err = e;
+        error = e;
       }
-      should.exist(err);
-      err.name.should.equal('TypeError');
-      err.message.should.equal(
-        '"source" must be an object or an array of objects.');
+      should.not.exist(error);
+      should.equal(state.deferredInstallPrompt, null);
+      should.equal(state.installCapabilities.inApp, false);
+      should.equal(state.installCapabilities.manual, false);
     });
-    it('should fail on empty params', async () => {
-      let err;
+    it('should fail', async () => {
+      let error;
       try {
-        extend();
+        initialize();
       } catch(e) {
-        err = e;
+        error = e;
       }
-      should.exist(err);
-      err.name.should.equal('TypeError');
+      should.not.exist(error);
+
+      try {
+        initialize();
+      } catch(e) {
+        error = e;
+      }
+      should.exist(error);
+      error.message.should.include('already initialized');
     });
-    it('should detect an object', async () => {
-      const result = isObject({});
-      result.should.equal(true);
+  });
+  describe('showInstallPrompt', () => {
+    beforeEach(() => {
+      // clear initialized state
+      state._initialized = false;
     });
-    it('should detect a non-object', async () => {
-      const result = isObject('');
-      result.should.equal(false);
+    it('should fail', async () => {
+      let error;
+      try {
+        initialize();
+        showInstallPrompt();
+      } catch(e) {
+        error = e;
+      }
+      should.exist(error);
+      error.message.should.include('No PWA install prompt available');
     });
-  }); // end API
+  });
 });
